1381eba8b3cdf9415f73edc7889243b2ce044312
Ravichandra <ravichandra.enaganti@gmail.com>
2015-05-27 16:29:30 +0530

    Java class for json info

---
+++ src/UserInfo.java
@@ -0,0 +1,25 @@
+/**
+ * Created by ravi on 5/27/15.
+ */
+public class UserInfo
+{
+    private int userId;
+    private String userName;
+
+    public int getUserId()
+    {
+        return userId;
+    }
+    public void setUserId(int userId)
+    {
+        this.userId = userId;
+    }
+    public String getUserName()
+    {
+        return userName;
+    }
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+}
